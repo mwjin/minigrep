d7eea1328a1bbda9c9107505e58d3fbfe1ca8753
Minwoo Jeong <jeongmwj@gmail.com>
2023-02-17 15:41:15 +0900

    Return a `Result` and handle the error instead of `panic`

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -20,13 +24,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Self {
+    fn build(args: &[String]) -> Result<Self, &'static str> {
         if args.len() < 3 {
-            panic!("at least two arguments are necessary")
+            return Err("at least two arguments are necessary");
         }
-        Self {
+        Ok(Self {
             query: args[1].clone(),
             file_path: args[2].clone(),
-        }
+        })
     }
 }
