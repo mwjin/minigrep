c4067998957078ec8a41ef9f0e89b39e787d49fe
Minwoo Jeong <jeongmwj@gmail.com>
2023-02-18 17:40:11 +0900

    Update tests

--- src/lib.rs
+++ src/lib.rs
@@ -57,17 +57,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 
     #[test]
-    fn one_result_case_insensitive() {
+    fn case_insensitive() {
         let query = "DUCT";
         let contents = "\
 Rust:
@@ -75,6 +76,21 @@ safe, fast, productive.
 Pick three.";
         assert_eq!(
             vec!["safe, fast, productive."],
+            search_case_insensitive(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive_multiple_results() {
+        let query = "RuSt";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
             search_case_insensitive(query, contents)
         );
     }
